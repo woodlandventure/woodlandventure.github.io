b16e6528b3473958c1306346ad28257326e02f00
Charlie <charliecrisp249@gmail.com>
2017-08-13 14:24:43 +0100

    Remove unused variables from index.js

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 var http = require('http');
-var url = require('url');
 var fs = require('fs');
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express(); 
-
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(bodyParser.json());
 
 app.use('/', express.static('public'));
 app.listen(8080);

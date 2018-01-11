09270c0345aba5415579baf5d93c2f351b66095b
Charlie Crisp <charliecrisp249@gmail.com>
2018-01-11 00:10:26 +0000

    Use css media queries not js in /contact

--- public/jquery/contactwindowresizer.js
+++
@@ -1,6 +0,0 @@
-$(window).resize(function () {
-    $('.find-top-item').css('padding-top', parseInt($('.navbar').css("height"))-20);
-});
-$(window).on('load', function () {
-    $('.find-top-item').css('padding-top', parseInt($('.navbar').css("height"))-20);
-});

5f24430f666770706acede7fdcf55dedba1f99b4
Charlie Crisp <charliecrisp249@gmail.com>
2018-01-11 10:11:14 +0000

    Use correct syntax to get license key

--- newrelic.js
+++ newrelic.js
@@ -1,10 +1,13 @@
 'use strict'
+require('dotenv').load();
 /**
  * New Relic agent configuration.
  *
  * See lib/config/default.js in the agent distribution for a more complete
  * description of configuration variables and their potential values.
  */
+
+let license = process.env.NEW_RELIC_LICENSE_KEY;
 exports.config = {
   /**
    * Array of application names.
@@ -13,7 +16,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key: ENV['NEW_RELIC_LICENSE_KEY'],
+  license_key: license,
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing

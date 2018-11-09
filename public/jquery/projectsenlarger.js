8c5fc3a6e89a80dda183b5dd8b9b7cefc4dbe09a
Charlie Crisp <charliecrisp249@gmail.com>
2018-11-09 22:51:11 +0000

    Add Basic ability to click onto shelter page

--- public/jquery/projectsenlarger.js
+++ public/jquery/projectsenlarger.js
@@ -14,9 +14,36 @@ for (var i = 0; i < images.length; i++) {
 }
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementById("close");
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() { 
   modal.style.display = "none";
+}
+
+//For images that take you to another page
+// Get the modal
+var modalClickable = document.getElementById('myModalClickable');
+
+var imagesClickable = document.getElementsByClassName("panel-image-clickable");
+var modalImgClickable = document.getElementById("modal-image-clickable");
+
+for (var i = 0; i < imagesClickable.length; i++) {
+  imagesClickable[i].onclick = function(){
+    console.log("clicked");
+    modalClickable.style.display = "block";
+    modalImgClickable.src = this.src;
+
+    var imageId = this.getAttribute("id");
+    var clickablehref = document.getElementById("clickablehref");
+    clickablehref.setAttribute("href", "/project/" + imageId + '.html');
+  }
+}
+
+// Get the <span> element that closes the modal
+var spanClickable = document.getElementById("closeClickable");
+
+// When the user clicks on <span> (x), close the modal
+spanClickable.onclick = function() { 
+  modalClickable.style.display = "none";
 }

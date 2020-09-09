249ca0284f9a01087dd93472cdb019711ebe63fc
Charlie Crisp <charliecrisp249@gmail.com>
2020-09-09 19:49:16 +0100

    No banner on project pages

--- public/jquery/includehtml.js
+++ public/jquery/includehtml.js
@@ -1,4 +1,5 @@
 $(function(){
       $("#navbar").load("/public/html/navbar.html");
+      $("#navbar-no-banner").load("/public/html/navbar_no_banner.html");
       $("#footer").load("/public/html/footer.html");
     });

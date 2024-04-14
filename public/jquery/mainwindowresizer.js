4265e8e2c62e9d74c773adcefd2c1b8388a7fe0a
Charlie Crisp <charliecrisp249@gmail.com>
2024-04-14 13:10:09 +0100

    Remove remaining party stuff and add IT Services

--- public/jquery/mainwindowresizer.js
+++ public/jquery/mainwindowresizer.js
@@ -2,16 +2,14 @@
 $(window).resize(function () {
     $('.top-item').css('padding-top', parseInt($('.navbar').css("height"))+10);
     if ($(window).width() < 768) {
-        $('#partypic').css('height', 200);
-        $('#grouppic').css('height', 300);
-        $('#corppic').css('height', 300);
+        $('#itservicespic').css('height', 300);
+        $('#projpic').css('height', 300);
         $('#sch1pic').css('height', 400);
         $('#sch2pic').css('height', 400);
 
     } else {
-        $('#partypic').css('height', $('#party').height());
-        $('#grouppic').css('height', $('#group').height());
-        $('#corppic').css('height', $('#corp').height());
+        $('#itservicespic').css('height', $('#itservices').height());
+        $('#projpic').css('height', $('#proj').height());
         $('#sch1pic').css('height', $('#sch1').height());
         $('#sch2pic').css('height', $('#sch2').height());
     }
@@ -20,16 +18,14 @@ $(window).resize(function () {
 $(window).on('load', function () {
     $('.top-item').css('padding-top', parseInt($('.navbar').css("height"))+10);
     if ($(window).width() < 768) {
-        $('#partypic').css('height', 200);
-        $('#grouppic').css('height', 300);
-        $('#corppic').css('height', 300);
+        $('#itservicespic').css('height', 300);
+        $('#projpic').css('height', 300);
         $('#sch1pic').css('height', 400);
         $('#sch2pic').css('height', 400);
 
     } else {
-        $('#partypic').css('height', $('#party').height());
-        $('#grouppic').css('height', $('#group').height());
-        $('#corppic').css('height', $('#corp').height());
+        $('#itservicespic').css('height', $('#itservices').height());
+        $('#projpic').css('height', $('#proj').height());
         $('#sch1pic').css('height', $('#sch1').height());
         $('#sch2pic').css('height', $('#sch2').height());
     }

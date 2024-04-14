cd3f297f59149c6fa8e4b2d48f457b801d45b416
Charlie Crisp <charliecrisp249@gmail.com>
2024-04-14 12:27:12 +0100

    Remove parties

--- public/jquery/includehtml.js
+++ public/jquery/includehtml.js
@@ -1,5 +1,4 @@
 $(function(){
       $("#navbar").load("/public/html/navbar.html");
-      $("#navbar-no-banner").load("/public/html/navbar_no_banner.html");
       $("#footer").load("/public/html/footer.html");
     });
